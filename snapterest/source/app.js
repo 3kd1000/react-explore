948e635990efb1a2432fa7866f9535bb0f486576
Ju Sang Jeong <3kd1000@gmail.com>
2020-01-16 09:32:08 +0900

    [first commit]

---
+++ snapterest/source/app.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class ReactClass extends Component {
+    render () {
+        return (
+            <h1 className="header"> React Component</h1>
+        );
+    }
+}
+
+const reactComponent = ReactDOM.render(
+    <ReactClass/>,
+    document.getElementById('react-application')
+);
+
+export default ReactClass;
+
